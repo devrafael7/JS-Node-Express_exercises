e4ccbd65f05f09c2a5d088a22fbb24ce50da0668
Rafael <rafarafa99944@gmail.com>
2025-01-01 22:28:52 -0300

    ex2 finished

--- ex2/app.js
+++ ex2/app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const url = require('url');
 
 function currentTimeOnServer(){
     const server = http.createServer((req, res)=>{
@@ -14,4 +15,31 @@ function currentTimeOnServer(){
 }
 
 //currentTimeOnServer()
+
+function sumAcrossNumberOnServer(){
+    const server = http.createServer((req, res)=>{
+        
+        const query = url.parse(req.url, true).query;
+        const num1 = parseFloat(query.num1);
+        const num2 = parseFloat(query.num2);
+
+        if(!isNaN(num1) && !isNaN(num2)){
+
+            const sum = num1 + num2;
+            res.writeHead(200, {'content-type': 'text/plain'});
+            res.end(`The sum of ${num1} and ${num2} is ${sum}`);
+        } else {
+            
+            res.writeHead(400, {'content-type': 'text/plain'});
+            res.end('Please enter a valid number to make a sum');
+        }
+
+    });
+
+    server.listen(8081, ()=>{
+        console.log('Server running at http://localhost:8081')
+    })
+}
+
+sumAcrossNumberOnServer()
 
