efffb77836da774907a9ca7894b153858b478d8e
Rafael <rafarafa99944@gmail.com>
2025-02-23 22:39:54 -0300

    Doing mathematical calculations in node

--- ex6/main.js
+++ ex6/main.js
@@ -107,18 +107,142 @@ function MaxAndMin(){
 //MaxAndMin();
 
 function PrimeNumber(num){
-    for (let i = 2; i <= num; i++){
-        let multiplierNumber = num / i
-        let primeNum = False
-        if (multiplierNumber % 2 == 0 && i != num){
-            console.log(`The number ${num} is not a prime number`);
-            let primeNum = True
-        } 
-        if (primeNum){
-            break;
+    if (num < 2) {
+        console.log(`${num} is not a prime number`);
+        return;
+    } 
+
+    for (let i = 2; i <= Math.sqrt(num); i ++){
+        if ( num % i === 0){
+            console.log(`${num} is not a prime number`);
+            return;
         }
     }
-    console.log(`The number ${num} is a prime number`)
+
+    console.log(`${num} is a prime number!`)
 }
 
-PrimeNumber(8)
+//PrimeNumber(7)
+
+//console.log(`Random number generated: ${Math.floor(Math.random() * 1) + 5}`);
+
+function areaOfACircle(radium){
+    const result = Math.PI * Math.pow(radium, 2);
+    console.log(result);
+}
+
+//areaOfACircle(7)
+
+function multipleNumber(num, multipleNum){
+    if (num % multipleNum === 0) {
+        console.log('its multiple');
+    } else {
+        console.log('its not multiple');
+    }
+};
+
+//multipleNumber(8, 3)
+
+function milesToKmConverter(miles){
+    let result = miles * 1.60934
+    console.log(`${result}`)
+}
+
+//milesToKmConverter(5)
+
+function ehPerfeito(num) {
+  let soma = 0;
+  for (let i = 1; i < num; i++) {
+    if (num % i === 0) soma += i;
+  }
+  return soma === num;
+}
+
+//ehPerfeito(3)
+
+function arithmeticMean(){
+    rl.question('Enter the numbers you want with space between them: ', (input)=>{
+        const numbers = input.split(" ").map(Number);
+
+        sum = 0
+
+        numbers.forEach(num =>{
+            sum += num
+        })
+
+        const result = sum / numbers.length;
+        console.log(`The result is ${result}`);
+  
+        rl.close();
+    })
+}
+
+//arithmeticMean();
+
+function foundANumberPositionPA(position){
+    let randomTerm = Math.floor((Math.random() * 10) + 1); 
+    for (let i = randomTerm; i <= 30; i += randomTerm){
+        console.log(i)
+    }
+    let NumberFound = randomTerm + (position - 1) * randomTerm
+    console.log(`The ${position} of the number in PA is ${NumberFound}`)
+}
+
+//foundANumberPositionPA(10);
+
+function sumPA(quantity, a1, an){
+    let randomTerm = Math.floor((Math.random() * 10) + 1); 
+    const PA = [];
+    for (let i = randomTerm; i <= 100; i += randomTerm){
+        console.log(i);
+        PA.push(i);
+    };
+    const sumOfPA = (quantity / 2) * (PA[a1 - 1] + PA[an - 1])
+    console.log(`The sum between the numbers of PA is ${sumOfPA}`)
+};
+
+//sumPA(3, 1, 3);
+
+function volumeOfSphere(r){
+    result = (4/3) * Math.PI * Math.pow(r, 3);
+    console.log(result);
+    process.exit();
+};
+
+//volumeOfSphere(12)
+
+function deltaCalc(a, b, c){
+    const result = Math.pow(b, 2) - 4 * a * c;
+    console.log(result);
+}
+
+//deltaCalc(2, 3, -2)
+
+function periOfACircle(radium){
+    const perimeter = 2 * Math.PI * parseFloat(radium);
+    console.log(perimeter);
+    process.exit();
+};
+
+//periOfACircle(3)
+
+function sumOfComplexNumbers(a, c, bi, di){
+    const z1 = a + bi
+    const z2 = c + di
+    const separation = z1 + " " + z2;
+    console.log(separation);
+};
+
+//sumOfComplexNumbers(2, 6, 2, 5);
+
+function discountCalc(){
+    const price = Math.floor(Math.random() * 1000) + 1;
+    let randomDiscount = Math.floor(Math.random() * 60) + 1;
+    const discount = (randomDiscount / 100);
+    const finalPrice = price - (price * discount);
+    console.log(`The original price is: ${price}`);
+    console.log(`The discount applied is: ${discount}`);
+    console.log(`The final price of the product is: ${finalPrice}`);
+}
+
+//discountCalc();
