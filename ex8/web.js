4273fc490fa25a030293553536019401658ce921
Rafael <rafarafa99944@gmail.com>
2025-04-21 17:33:47 -0300

    adding json file to save users information

--- ex8/web.js
+++ ex8/web.js
@@ -1,3 +1,7 @@
+const fs = require("fs");
+
+/*
+
 let shoppingCart = [];
 
 let qtdItem = (Math.random(10) * 10).toFixed(0);
@@ -46,18 +50,109 @@ function RemoveItemByName(product){
 };
 
 //RemoveItemByName("Ring");
+*/
+
+function SaveCartToFile(){
+    fs.writeFileSync("cart.json", JSON.stringify(cartByNameAndPrice, null, 2));
+};
 
 let cartByNameAndPrice = [];
 
 function AddToCartByNameAndPrice({product, price}){
     cartByNameAndPrice.push({product, price});
+    SaveCartToFile();
     console.log(cartByNameAndPrice);;
 };
 
-AddToCartByNameAndPrice({product: "earring", price: 29.99});
-AddToCartByNameAndPrice({product: "collar", price: 72});
+AddToCartByNameAndPrice({product: "Earring", price: 29.99});
+AddToCartByNameAndPrice({product: "Collar", price: 72});
+AddToCartByNameAndPrice({product: "Hat", price: 15});
+AddToCartByNameAndPrice({product: "Pants", price: 59.99});
 
+function RemoveItemByNameAndPrice({product}){
+    cartByNameAndPrice = cartByNameAndPrice.filter(cartItem => {
+        return cartItem.product.toLowerCase() !== product.toLowerCase();
+    });
+    SaveCartToFile();
+    console.log(cartByNameAndPrice);
+};
 
+RemoveItemByNameAndPrice({product: "hat"});
 
+let totalPrice = 0;
+function FindTotalPrice(){
+    totalPrice = cartByNameAndPrice.reduce((total, item)=>{
+        return total + item.price;
+    },0);
+    console.log(totalPrice);
+};
 
+FindTotalPrice();
 
+let userPoints = 0;
+let discountApplied = false;
+const balance = (Math.random(10) * 50).toFixed(2);
+function CheckBalance(){
+    console.log(balance);
+    let isSufficient = false;
+    if (balance >= totalPrice){
+        isSufficient = true;
+    } else {
+        isSufficient;
+    };
+    
+    const balanceStatus = isSufficient ? `Sufficient Balance`: `Insufficient Balance`;
+    console.log(balanceStatus);
+
+    let finalBalance = 0;
+    function BuyProducts(){
+        if (isSufficient){
+            finalBalance = balance - totalPrice;
+            if (discountApplied){
+                finalBalance -= userPoints;
+            }
+            console.log(`Purchase made successfully! Your current balance is: ${(finalBalance).toFixed(2)}`);
+        } else {
+            console.log(`It needs ${(totalPrice - balance).toFixed(2)} to make the purchase`);
+        };
+
+                
+        function EarnPointsPerPurchase(){
+            userPoints += Math.floor(totalPrice / 10);
+            console.log(`You have ${userPoints}`);
+        };
+
+        function ApplyDiscount(){
+            discountApplied = true;            
+        }
+
+        if (isSufficient){
+            EarnPointsPerPurchase();
+        }; 
+
+        if (isSufficient){
+            ApplyDiscount();
+        }; 
+
+    };
+
+    BuyProducts();
+};
+
+CheckBalance();
+
+function SearchProduct({productName}){
+    return cartByNameAndPrice.some(item => item.product === productName) ? `Product Found` : `Product Not Found`;
+};
+
+//console.log(SearchProduct({productName: "earring"}));
+
+function SearchProductByKeyword({product}){
+    const lowerKeyword = product.toLowerCase();
+
+    const filteredKeyword = cartByNameAndPrice.filter(item => item.product.toLowerCase().includes(lowerKeyword));
+    return filteredKeyword.length ? filteredKeyword: `Product not found by keyword`;
+};
+
+console.log(SearchProductByKeyword({product: "earr"}));
+console.log(SearchProductByKeyword({product: "p"}));
