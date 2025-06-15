fe220ed696585a80f57191f4106479e7de3580f6
Rafael <rafarafa99944@gmail.com>
2025-06-15 19:17:50 -0300

    adding ex12

---
+++ ex12/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+//1
+const totalSides = 6;
+const greaterThan4 = [5, 6];
+console.log((greaterThan4.length / totalSides) * 100 + "%");
---
+++ ex12/src/index.ts
@@ -0,0 +1,5 @@
+//1
+
+const totalSides: number = 6;
+const greaterThan4: number[] = [5, 6];
+console.log((greaterThan4.length/totalSides) * 100 + "%");
