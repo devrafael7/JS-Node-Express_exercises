376fa0820f2aec269e9d4dae7b58194681786f36
Rafael <rafarafa99944@gmail.com>
2025-07-03 21:06:19 -0300

    ex13

--- ex13/dist/math.js
+++ ex13/dist/math.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log("hello world!");
+//Factorial
+function Factorial(n) {
+    return n <= 1 ? 1 : n * Factorial(n - 1);
+}
+;
+//console.log(Factorial(5))
+//Combination
+function Combination(n, k) {
+    return Factorial(n) / (Factorial(k) * Factorial(n - k));
+}
+;
+console.log(Combination(5, 2));
--- ex13/src/math.ts
+++ ex13/src/math.ts
@@ -1,1 +1,23 @@
-console.log("hello world!")
+//Factorial
+
+function Factorial(n: number): number {
+    return n <= 1 ? 1 : n * Factorial(n - 1);
+};
+
+//console.log(Factorial(5))
+
+//Combination
+
+function Combination(n: number, k: number) : number {
+    return Factorial(n) / (Factorial(k) * Factorial(n - k));
+};
+
+//console.log(Combination(5,2))
+
+//Permutation with repetition
+
+//const result = Factorial(n) / (Factorial(p0) * Factorial(p1) * Factorial(p2)...); 
+
+
+
+
