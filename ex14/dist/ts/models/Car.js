16a6c22d29af9f47bc5bf78501db70e74053d9e1
Rafael <rafarafa99944@gmail.com>
2025-07-24 19:34:52 -0300

    class Car has been created

---
+++ ex14/dist/ts/models/Car.js
@@ -0,0 +1,23 @@
+"use strict";
+class Car {
+    //Instance
+    constructor(brand, model) {
+        this.speed = 0; //public String speed
+        this.brand = brand;
+        this.model = model;
+    }
+    accelerate() {
+        this.speed += 10;
+        console.log(`Accelerating. Speed: ${this.speed} km/h`);
+    }
+    ;
+    brake() {
+        this.speed = Math.max(10, this.speed - 10);
+        console.log(`Braking. Speed: ${this.speed} km/h`);
+    }
+    ;
+}
+;
+const Car1 = new Car("Toyota", "Corolla");
+Car1.accelerate();
+Car1.brake();
---
+++ ex14/src/ts/models/Car.ts
@@ -0,0 +1,25 @@
+class Car { //public class Car
+    brand: string; //public String brand
+    model: string; //public String model
+    speed: number = 0; //public String speed
+
+    //Instance
+    constructor(brand: string, model:string){ //public Car(String brand, String model, String speed)
+        this.brand = brand; 
+        this.model = model;
+    }
+
+    accelerate(): void {
+        this.speed += 10;
+        console.log(`Accelerating. Speed: ${this.speed} km/h`);
+    };
+
+    brake(): void {
+        this.speed = Math.max(0, this.speed - 10);
+        console.log(`Braking. Speed: ${this.speed} km/h`);
+    };
+};
+
+const Car1 = new Car("Toyota", "Corolla");
+Car1.accelerate();
+Car1.brake();
